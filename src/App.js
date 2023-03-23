7c4aefadf53c7cfc15b1d96d4070a99fe7f92c1d
Fabian <hahn.fabian@proton.me>
2023-03-23 23:41:44 +0100

    Topnav -> flexbox

--- src/App.js
+++ src/App.js
@@ -25,18 +25,25 @@ function App() {
         <div className="wrapper">
             <div className='header'>
               <div className='topnav'>
-                <span><img src={threeDots} alt="Three dots"></img></span>
-                <span><img src={star} alt="Star"></img></span>
-                <span><img src={clock} alt="Clock"></img></span>
-                <span><img src={comment} alt="Comment"></img></span>
-                <span>Share</span>
-                <span>Edited {edited}</span>
-                <span style={{float: 'left'}}>pageName</span>
+                <div style={{marginRight:"auto"}}>
+                  <span>pageName</span>
+                </div>
+                <div className='homepage' >
+                  <a style={{color: 'inherit', textDecoration:"none",}} href='https://johannesfabianhahn.de/en/index.html'>HOME</a>
+                </div>
+                <div className='testWrapper'>
+                  <span className='floatLeft'>Edited {edited}</span>
+                  <span className='floatLeft'>Share</span>
+                  <span><img src={comment} alt="Comment"></img></span>
+                  <span><img src={clock} alt="Clock"></img></span>
+                  <span><img src={star} alt="Star"></img></span>
+                  <span className='floatLeft'><img src={threeDots} alt="Three dots"></img></span>
+                </div>
               </div>
             </div>
             <div className='menu' >
               <div className='menuWrapper'>
-                <div className='menu-twoSpaces'>Notizen</div>
+                <div className='menu-twoSpaces'>📝 My Notes</div>
                 <div className='menu-list'> 
                   <img src={magnfyingGlass} alt="Search" ></img>
                   <span>Search</span>
@@ -51,14 +58,16 @@ function App() {
                 </div>
                 <div className='menu-list'>
                   <img src={settings} alt="Settings" ></img>
-                  <span>Settings & members</span>
+                  <span>Settings</span>
                 </div>
                 <div className='menu-twoSpaces' style={{paddingTop: "25px"}}>Private</div>
                 <div className='menu-list'> Page List Here
                 </div> 
                 <div className='menu-list-bottom'>
-                  <img src={plus} alt="Plus"></img>
-                  <span>New Page</span>
+                    <div style={{marginLeft:"5px"}}>
+                      <img src={plus} alt="Plus"></img>
+                      <span>New Page</span>
+                    </div>
                 </div>
               </div>
             </div>
--- src/page.js
+++ src/page.js
@@ -2,13 +2,22 @@ import "./css/page.css";
 
 export default function page() {
     return (
-        <div className="main-wrapper">
-            
-                <div>
-                    <textarea type={"text"}></textarea>
+        <div>
+            <div style={{display:"flex", width:"100%", justifyContent:"center", flexShrink:"0"}}>
+                <div style={{width:"900"}}>
+                    <div>
+                        AAA
+                    </div>
                 </div>
-                
+            </div>
+            <div style={{display:"flex", width:"100%", justifyContent:"center"}}>
+                    <div style={{maxWidth: "100%", minWidth:"0px", width:"900px" }}>
+                        <div style={{width:"100%", display:"flex", justifyContent:"center", flexShrink:"0"}}>
+                            aaaa
+
+                        </div>
+                    </div>
+            </div>
         </div>
-        
     )
 }
